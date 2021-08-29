b390bdb6a6f7a41e0a46288ecacdb73c0ddb7911
vgomesdcc <40807939+vgomesdcc@users.noreply.github.com>
2021-08-28 21:35:13 -0300

    Update p5.c

--- p5.c
+++ p5.c
@@ -8,9 +8,9 @@ int main() {
   scanf("%d",&numero);
 
   if(numero%2 == 0){
-		printf("o numero e par\n");
+	printf("o numero e par\n");
 	}
 
-	else
-    printf("o numero e impar\n");
+   else
+   	printf("o numero e impar\n");
 }
