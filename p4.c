d3f1b322151f934200204616be3482ac4bcec2af
vgomesdcc <40807939+vgomesdcc@users.noreply.github.com>
2021-09-11 13:52:56 -0300

    Update p4.c

--- p4.c
+++ p4.c
@@ -2,18 +2,30 @@
 
 int main() {
 
-  printf("Digite dois numeros: "); 
+  printf("Insira os numeros: \n");
 
-  int numerador;
-  scanf("%d",&numerador);
+  int perfeito1;
+  scanf("%d",&perfeito1);
+  int perfeito2; 
+  scanf("%d",&perfeito2); 
 
-  int denominador;
-  scanf("%d",&denominador);
+  int flag1=0;
+  int flag2=0;
 
-  if(numerador%denominador == 0){
-		printf("Divisores perfeitos\n");
+  for(int i=1; i<perfeito1; i++){
+    if(perfeito1%i==0){
+			flag1+=i;
+		}
+  }
+
+  for(int z=1; z<perfeito2; z++){
+		if(perfeito2%z==0){
+			flag2+=z;
+		}
 	}
-
-	else
+  if(perfeito1==flag1 && perfeito2==flag2){
+    printf("Sao divisores perfeitos\n");
+  }
+  else
     printf("Nao sao divisores perfeitos\n");
 }
